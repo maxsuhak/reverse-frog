0f60c69a2c7c05f711c6818235cdca53f9dc0e28
maxsuhak <maxsuhak@gmail.com>
2013-12-18 12:46:36 +0200

    added frog specs

--- frog/frog.rb
+++ frog/frog.rb
@@ -1,39 +1,15 @@
-# def frog(x, a)
-#   return 'False attributes' unless a.is_a?(Array) &&
-#                                   a.size.between?(1, 100_000) &&
-#                                   x.is_a?(Integer) &&
-#                                   x <= a.max &&
-#                                   x.between?(1, 100_000)
-#   return -1 unless a.include?(x)
-#   rez = (1..x).to_a - a
-#   return rez.empty? ? a.index(x) : -1
-# end
-
-def should_start_crossing_at(leafs_num, hash)
-    raise "must be NxM hash" unless hash.is_a?(Hash) # hash is a hash!
-
-    # The solution is simple
-    # First we reverse the hash so the keys are the leafs and values are minutes
-    reverse_hash = {}
-    hash.each do |time, leaf|
-      reverse_hash[leaf] ||= []
-      reverse_hash[leaf] << time
+class Frog
+    def self.crossing(leafs_num, hash)
+        raise "must be hash" unless hash.is_a?(Hash)
+        raise "must be int" unless leafs_num.is_a?(Integer)
+        raise "false attributes" unless leafs_num.between?(1, 100_000)
+        raise "false attributes" unless hash.size.between?(1, 100_000)
+        reverse_hash = {}
+        hash.each do |time, leaf|
+          reverse_hash[leaf] ||= []
+          reverse_hash[leaf] << time
+        end
+        return -1 if (1..leafs_num).to_a.any? { |n| reverse_hash[n].nil?  || reverse_hash[n].empty? }
+        reverse_hash.values.map(&:min).flatten.max
     end
-
-    # There's a leaf that will never fall from the tree so frog will die
-    # In this case return -1 (it should raise FrogDeath error in my opinion)
-    return -1 if (1..leafs_num).to_a.any? { |n| reverse_hash[n].nil?  || reverse_hash[n].empty? }
-
-    # reverse_hash will return this values:
-    # leaf minutes_sorted_asc
-    # 1    0 2
-    # 2    4
-    # 3    1 5
-    # 4    3 7
-    # 5    6
-
-    # We take the first row and return maximum, that will be the answer
-    reverse_hash.values.map(&:min).flatten.max
 end
-
-puts should_start_crossing_at(5, {0=>1, 1=>3, 2=>1, 3=>4, 4=>2, 5=>3, 6=>5, 7=>4})
--- frog/frog_spec.rb
+++ frog/frog_spec.rb
@@ -1,2 +1,59 @@
 require_relative 'frog.rb'
 require 'rspec'
+
+describe Array do
+
+  let(:hash) { {0=>1, 1=>3, 2=>1, 3=>4, 4=>2, 5=>3, 6=>5, 7=>4} }
+  let(:not_valid_hash) { (1..100_001).each_with_index { |h, i| not_valid_hash[i] = h } }
+  let(:empty_hash) { {} }
+  let(:x) { 5 }
+  let(:not_valid_x) { 100_001 }
+
+  context 'with ivalid attrebutes' do
+    it 'should raise if value is not hash' do
+      lambda { Frog.crossing(x, :not_a_hash) }.should raise_error
+    end
+
+    it 'should raise if value is not int' do
+      lambda { Frog.crossing(:not_a_integer, hash) }.should raise_error
+    end
+
+    it 'is not valid when x more than 100_000' do
+      lambda { Frog.crossing(not_valid_x, hash) }.should raise_error
+    end
+
+    it 'is not valid when hash more than 100_000' do
+      lambda { Frog.crossing(x, not_valid_hash) }.should raise_error
+    end
+
+    it 'is not valid when hash is empty' do
+      lambda { Frog.crossing(x, empty_hash) }.should raise_error
+    end
+  end
+
+  context 'with valid attrebutes' do
+    it 'should find the lowest time as in example' do
+      Frog.crossing(x, hash).should eq(6)
+    end
+
+    it 'is return 2' do
+      a = {0=>1, 1=>2, 2=>3}
+      Frog.crossing(3, a).should eq(2)
+    end
+
+    it 'is return 4' do
+      a = {0=>1, 1=>24, 2=>3, 3=>2, 4=>3}
+      Frog.crossing(3, a).should eq(3)
+    end
+
+    it 'x is not match' do
+      a = {0=>1, 1=>2, 2=>4, 3=>5}
+      Frog.crossing(3, a).should eq(-1)
+    end
+    
+    it 'is return -1' do
+      Frog.crossing(6, hash).should eq(-1)
+    end
+  end
+
+end
